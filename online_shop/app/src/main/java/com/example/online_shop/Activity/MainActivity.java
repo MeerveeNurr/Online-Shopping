b1bab84f3f2b7ab52551effa9e586761535d26b7
MeerveeNurr <mervenurrkk@gmail.com>
2025-04-23 17:22:38 +0300

    Online-Shopping
    An online shopping application using Java, Firebase Realtime Database, Android SDK

---
+++ online_shop/app/src/main/java/com/example/online_shop/Activity/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.online_shop.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+
+import com.example.online_shop.Adapter.CategoryAdapter;
+import com.example.online_shop.Adapter.PopularAdapter;
+import com.example.online_shop.Adapter.SliderAdapter;
+import com.example.online_shop.Domain.CategoryDomain;
+import com.example.online_shop.Domain.ItemsDomain;
+import com.example.online_shop.Domain.SliderItems;
+import com.example.online_shop.R;
+import com.example.online_shop.databinding.ActivityMainBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BaseActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initBanner();
+        initCategory();
+        initPopular();
+        bottomNavigation();
+
+    }
+
+    private void bottomNavigation() {
+        binding.cartBtn.setOnClickListener(v -> startActivity(new Intent(MainActivity.this,CartActivity.class)));
+    }
+
+    private void initPopular() {
+        DatabaseReference myref=database.getReference("Items");
+        binding.progressBarPopular.setVisibility(View.VISIBLE);
+        ArrayList<ItemsDomain> items=new ArrayList<>();
+
+        myref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot issue:snapshot.getChildren()){
+                        items.add(issue.getValue(ItemsDomain.class));
+                    }
+                    if(!items.isEmpty()) {
+                        binding.recycleviewPopular.setLayoutManager(new GridLayoutManager(MainActivity.this,2));
+                        binding.recycleviewPopular.setAdapter(new PopularAdapter(items));
+                    }
+                    binding.progressBarPopular.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initCategory() {
+        DatabaseReference myref=database.getReference("Category");
+        binding.progressBarOfficial.setVisibility(View.VISIBLE);
+        ArrayList<CategoryDomain> items = new ArrayList<>();
+        myref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot issue:snapshot.getChildren()){
+                        items.add(issue.getValue(CategoryDomain.class));
+                    }
+                    if(!items.isEmpty()){
+                        binding.recyclerViewOfficial.setLayoutManager(new LinearLayoutManager(MainActivity.this,
+                                LinearLayoutManager.HORIZONTAL,false));
+                        binding.recyclerViewOfficial.setAdapter(new CategoryAdapter(items));
+
+                    }
+                    binding.progressBarOfficial.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void initBanner() {
+        DatabaseReference myRef=database.getReference("Banner");
+        binding.progressBarBanner.setVisibility(View.VISIBLE);
+        ArrayList<SliderItems> items=new ArrayList<>();
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot issue:snapshot.getChildren()){
+                        items.add(issue.getValue(SliderItems.class));
+                    }
+                    banners(items);
+                    binding.progressBarBanner.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+    private void banners(ArrayList<SliderItems> items) {
+        binding.viewpagerSlider.setAdapter((new SliderAdapter(items,binding.viewpagerSlider)));
+        binding.viewpagerSlider.setClipToPadding(false);
+        binding.viewpagerSlider.setClipChildren(false);
+        binding.viewpagerSlider.setOffscreenPageLimit(3);
+        binding.viewpagerSlider.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+
+        binding.viewpagerSlider.setPageTransformer(compositePageTransformer);
+    }
+}
